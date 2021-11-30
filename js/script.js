f088b1bff6ad258a03282f7ec0ee476657279a3a
Qingyuan Chen <Qingyuan.Chen2@georgebrown.ca>
2021-11-30 14:56:57 -0500

    first push

--- js/script.js
+++ js/script.js
@@ -2,12 +2,74 @@
 // If no argument is passed, 6 is set, ie, returns a number between and including 1 to 6
 // 		For example: 
 //    getRandomDiceRoll() will return either 1, 2, 3, 4, 5, 6
+let lastRoll = 0;
+let rollNum = 0;
 const getRandomDiceRoll = function(sides=6) {
   return Math.floor( Math.random() * sides ) + 1
 }
 
+const diceroll = function(){
+  const rollNum = getRandomDiceRoll();
+  let stringnum = '';
+  const elemessage = document.querySelector(`#rollnum`);
+
+  switch(rollNum){
+    case 1:
+      stringnum = 'One';
+      break;
+    case 2:
+      stringnum = 'Two';
+      break;
+    case 3:
+      stringnum = 'Three';
+      break;
+    case 4:
+      stringnum = 'Four';
+      break;
+    case 5:
+      stringnum = 'Five';
+      break;
+    case 6:
+      stringnum = 'Six';
+      break;
+  }
+  elemessage.textContent = `${stringnum}`;
+  const $Face = document.querySelector(`#face`);
+  $Face.setAttribute(`src`,`img/dice${rollNum}.svg`);
+  $Face.setAttribute(`alt`,`dice${rollNum}`)
+  return rollNum
+}
+
+
+function comparison(){
+  rollNum =  diceroll()
+  if(rollNum > lastRoll){
+    document.getElementById('last').innerHTML = `${lastRoll}`
+    document.getElementById('now').innerHTML = `${rollNum}`
+    document.getElementById('win').innerHTML = `You Win`;
+    lastRoll = rollNum;
+  }
+  else if(rollNum === lastRoll){
+    document.getElementById('win').innerHTML = `You Draw`;
+    document.getElementById('last').innerHTML = `${lastRoll}`
+    document.getElementById('now').innerHTML = `${rollNum}`
+    lastRoll = rollNum;
+  }
+  else{
+    document.getElementById('win').innerHTML = `You Lost`;
+    document.getElementById('last').innerHTML = `${lastRoll}`
+    document.getElementById('now').innerHTML = `${rollNum}`
+    lastRoll = rollNum;
+  }
+
+}
+
+const eleRoll = document.querySelector(`#roll`)
+eleRoll.addEventListener(`click`,diceroll);
+eleRoll.addEventListener(`click`, comparison)
+
+
 // Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
 
